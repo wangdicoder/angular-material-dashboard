016f052e41d84ba064d13c90564de2ac97d840b3
Di Wang <wangd7@student.unimelb.edu.au>
2017-05-31 22:53:29 +1000

    fix side bar issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,8 +8,8 @@ import { TableComponent } from './dashboard/table/table.component';
 import { NotificationComponent } from './dashboard/notification/notification.component';
 
 const routes: Routes = [
-  // {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  // {path: 'home', component: HomeComponent},
   {path: 'profile', component: ProfileComponent},
   {path: 'table', component: TableComponent},
   {path: 'notification', component: NotificationComponent}
