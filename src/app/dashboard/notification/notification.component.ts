ca9c42393cd96ddc978dc7431917a9ef55007a7f
Di Wang <wangd7@student.unimelb.edu.au>
2017-06-14 17:36:54 +1000

    notification page

--- src/app/dashboard/notification/notification.component.ts
+++ src/app/dashboard/notification/notification.component.ts
@@ -13,15 +13,31 @@ export class NotificationComponent implements OnInit {
   ngOnInit() {
   }
 
-  btnNotify() {
+  btnClick(position) {
+    let from = 'top';
+    let align = 'right';
+    switch (position) {
+      case 'top-left':
+        align = 'left';
+        break;
+      case 'top-center':
+        align = 'center';
+        break;
+      case 'bottom-left':
+        align = 'left';
+        from = 'bottom';
+        break;
+      case 'bottom-center':
+        align = 'center';
+        from = 'bottom';
+        break;
+      case 'bottom-right':
+        from = 'bottom';
+        break;
+    }
     $.notify({
       message: 'Welcome to <b>MATERIAL DASHBOARD</b> - a beautiful dashboard for every web developer.'
-    }, {
-      placement: {
-        from: 'top',
-        align: 'left'
-      }
-    });
+    }, {placement: {from, align}});
   }
 
 }
