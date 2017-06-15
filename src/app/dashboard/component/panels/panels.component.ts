5fc787d3df47a6db54c04094ad722b09e2b026b2
Di Wang <wangd7@student.unimelb.edu.au>
2017-06-15 13:08:00 +1000

    vertical panel

--- src/app/dashboard/component/panels/panels.component.ts
+++ src/app/dashboard/component/panels/panels.component.ts
@@ -6,23 +6,37 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   styleUrls: ['./panels.component.css']
 })
 export class PanelsComponent implements OnInit, AfterViewInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
   ngAfterViewInit() {
-    const activeTab = <HTMLElement>document.getElementsByClassName('default-active')[0];
-    activeTab.click();
+    const activeTabs = document.getElementsByClassName('default-active');
+    for (let i = 0; i < activeTabs.length; i++) {
+      (<HTMLElement>activeTabs[i]).click();
+    }
   }
 
   tabClick(evt, id) {
-    const tabcontents = document.getElementsByClassName('tab-content');
+    const tabcontents = document.querySelectorAll('.h-tab .tab-content');
     for (let i = 0; i < tabcontents.length; i++) {
-      const tabcontent = <HTMLElement>tabcontents[i];
-      tabcontent.style.display = 'none';
+      (<HTMLElement>tabcontents[i]).style.display = 'none';
     }
-    const tablinks = document.getElementsByClassName('tab-link');
+    const tablinks = document.querySelectorAll('.h-tab .tab-link');
+    for (let i = 0; i < tablinks.length; i++) {
+      const tablink = <HTMLElement>tablinks[i];
+      tablink.className = tablink.className.replace(' active', '');
+    }
+    document.getElementById(id).style.display = 'block';
+    evt.currentTarget.className += ' active';
+  }
+  vTabClick(evt, id) {
+    const tabcontents = document.querySelectorAll('.v-tab .tab-content');
+    for (let i = 0; i < tabcontents.length; i++) {
+      (<HTMLElement>tabcontents[i]).style.display = 'none';
+    }
+    const tablinks = document.querySelectorAll('.v-tab .tab-link');
     for (let i = 0; i < tablinks.length; i++) {
       const tablink = <HTMLElement>tablinks[i];
       tablink.className = tablink.className.replace(' active', '');
