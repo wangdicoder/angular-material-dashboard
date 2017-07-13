26abf051a16ba7a91d5a0edb7c8fb0c55be98286
Di Wang <wangd7@student.unimelb.edu.au>
2017-07-13 21:47:16 +1000

    settings page - add white bg

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,14 +11,18 @@ export class SidebarComponent implements OnInit, AfterViewInit, OnDestroy {
   public color: string;
   public menuItems: object;
   public activeFontColor: string;
-  constructor(public setingsService: SettingsService) {
+  public normalFontColor: string;
+  public dividerBgColor: string;
+  constructor(public settingsService: SettingsService) {
     this.menuItems = ROUTES;
     this.activeFontColor = 'rgba(0,0,0,.6)';
+    this.normalFontColor = 'rgba(255,255,255,.8)';
+    this.dividerBgColor = 'rgba(255, 255, 255, 0.5)';
   }
 
   ngOnInit() {
-    this.color = this.setingsService.getSidebarFilter();
-    this.setingsService.sidebarFilterUpdate.subscribe((filter: string) => {
+    this.color = this.settingsService.getSidebarFilter();
+    this.settingsService.sidebarFilterUpdate.subscribe((filter: string) => {
       this.color = filter;
       if (filter === '#fff') {
         this.activeFontColor = 'rgba(0,0,0,.6)';
@@ -26,9 +30,18 @@ export class SidebarComponent implements OnInit, AfterViewInit, OnDestroy {
         this.activeFontColor = 'rgba(255,255,255,.8)';
       }
     });
+    this.settingsService.sidebarColorUpdate.subscribe((color: string) => {
+      if (color === '#fff') {
+        this.normalFontColor = 'rgba(0,0,0,.6)';
+        this.dividerBgColor = 'rgba(0,0,0,.1)';
+      }else {
+        this.normalFontColor = 'rgba(255,255,255,.8)';
+        this.dividerBgColor = 'rgba(255, 255, 255, 0.5)';
+      }
+    });
   }
   ngOnDestroy() {
-    this.setingsService.sidebarFilterUpdate.unsubscribe();
+    this.settingsService.sidebarFilterUpdate.unsubscribe();
   }
 
   ngAfterViewInit() {
